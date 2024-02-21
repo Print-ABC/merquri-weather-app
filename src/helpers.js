593f216a81f6195be55ef39cab3811acce2b2df1
Tan Suan Khai <mail.abcwxyz@gmail.com>
2024-02-21 18:49:28 +0800

    Add helpers.js

---
+++ src/helpers.js
@@ -0,0 +1,25 @@
+const API_KEY = '4194cfa249cb9bf8ea8ea06395a6a3a0'
+const UNIT = 'metric'
+
+export const DEFAULT_SEARCH_QUERY = 'Singapore'
+
+export const GetApiEndPoint= (searchData) => {
+    return `http://api.openweathermap.org/data/2.5/weather?q=${searchData}&appid=${API_KEY}&units=${UNIT}`
+}
+
+export function GetCurrentTimeStamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = now.getHours();
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const meridiem = hours >= 12 ? 'pm' : 'am';
+    const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+
+    return `${day}-${month}-${year} ${formattedHours}:${minutes}${meridiem}`;
+}
+
+export function RoundNearestWholeNum(value) {
+    return Math.round(value);
+}
