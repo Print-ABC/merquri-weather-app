121fad7db7a0e5123da6cc6c46bd218d2b9074db
Tan Suan Khai <mail.abcwxyz@gmail.com>
2024-02-27 16:55:26 +0800

    Remove api key from code

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,9 @@
-const API_KEY = 'ecf32d84311243a895766cd81f14a6e2'
 const UNIT = 'metric'
 
 export const DEFAULT_SEARCH_QUERY = 'Singapore'
 
 export const GetApiEndPoint= (searchData) => {
-    return `http://api.openweathermap.org/data/2.5/weather?q=${searchData}&appid=${API_KEY}&units=${UNIT}`
+    return `http://api.openweathermap.org/data/2.5/weather?q=${searchData}&appid=${process.env.REACT_APP_API_KEY_OPENWEATHER}&units=${UNIT}`
 }
 
 export function GetCurrentTimeStamp() {
